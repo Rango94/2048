bd13305221d37ae052c8f0d2c79fd3e272d442f3
NanZhi Wang <wangnz1994@163.com>
2018-07-08 23:42:39 +0800

    2048

---
+++ Map.py
@@ -0,0 +1,132 @@
+import numpy as np
+import random as rd
+
+class map:
+    def __init__(self):
+        self.Matrix=np.zeros((4,4))
+        self.Matrix[rd.randint(0,3),rd.randint(0,3)]=2
+        self.history_vector=[]
+        self.history_step = []
+    def move_right(self):
+        self.history_vector.append(self.get_vector())
+        for i in range(4):
+            tmp=[ii for ii in self.Matrix[i] if ii!=0]
+            ii=len(tmp)-1
+            while ii>0:
+                if tmp[ii]==tmp[ii-1]:
+                    tmp[ii]=tmp[ii]*2
+                    tmp[ii-1]=0
+                ii-=1
+            tmp = [ii for ii in tmp if ii != 0]
+            self.Matrix[i,4-len(tmp):4]=np.array(tmp)
+            self.Matrix[i,0:4 - len(tmp)] = 0
+        while True:
+            rd1=rd.randint(0, 3)
+            rd2=rd.randint(0, 3)
+            if self.Matrix[rd1,rd2 ] == 0:
+                self.Matrix[rd1,rd2 ]=2
+                break
+        self.history_step.append(np.array([0,0,0,1]))
+        return self.check_over()
+
+    def move_left(self):
+        self.history_vector.append(self.get_vector())
+        for i in range(4):
+            tmp=[ii for ii in self.Matrix[i] if ii!=0]
+            ii=0
+            while ii<len(tmp)-1:
+                if tmp[ii]==tmp[ii+1]:
+                    tmp[ii]=tmp[ii]*2
+                    tmp[ii+1]=0
+                ii+=1
+            tmp = [ii for ii in tmp if ii != 0]
+            self.Matrix[i,len(tmp):4]=0
+            self.Matrix[i,0:len(tmp)] = np.array(tmp)
+        while True:
+            rd1=rd.randint(0, 3)
+            rd2=rd.randint(0, 3)
+            if self.Matrix[rd1,rd2 ] == 0:
+                self.Matrix[rd1,rd2 ]=2
+                break
+
+        self.history_step.append(np.array([0,0,1,0]))
+        return self.check_over()
+
+    def move_down(self):
+        self.history_vector.append(self.get_vector())
+        for i in range(4):
+            tmp=[ii for ii in self.Matrix[:,i] if ii!=0]
+            ii=len(tmp)-1
+            while ii>0:
+                if tmp[ii]==tmp[ii-1]:
+                    tmp[ii]=tmp[ii]*2
+                    tmp[ii-1]=0
+                ii-=1
+            tmp = [ii for ii in tmp if ii != 0]
+            self.Matrix[4-len(tmp):4,i]=np.array(tmp)
+            self.Matrix[0:4 - len(tmp),i] = 0
+        while True:
+            rd1=rd.randint(0, 3)
+            rd2=rd.randint(0, 3)
+            if self.Matrix[rd1,rd2 ] == 0:
+                self.Matrix[rd1,rd2 ]=2
+                break
+
+        self.history_step.append(np.array([0,1,0,0]))
+        return self.check_over()
+
+    def move_up(self):
+        self.history_vector.append(self.get_vector())
+        for i in range(4):
+            tmp=[ii for ii in self.Matrix[:,i] if ii!=0]
+            ii = 0
+            while ii < len(tmp) - 1:
+                if tmp[ii] == tmp[ii + 1]:
+                    tmp[ii] = tmp[ii] * 2
+                    tmp[ii + 1] = 0
+                ii += 1
+            tmp = [ii for ii in tmp if ii != 0]
+            self.Matrix[len(tmp):4,i] = 0
+            self.Matrix[0:len(tmp),i] = np.array(tmp)
+        while True:
+            rd1=rd.randint(0, 3)
+            rd2=rd.randint(0, 3)
+            if self.Matrix[rd1,rd2 ] == 0:
+                self.Matrix[rd1,rd2 ]=2
+                break
+
+        self.history_step.append(np.array([1,0,0,0]))
+        return self.check_over()
+
+    def check_over(self):
+        for i in self.Matrix:
+            for j in i:
+                if j==0:
+                    return False
+        return True
+
+    def get_vector(self):
+        out=[]
+        for i in self.Matrix:
+            for j in i:
+                out.append(j)
+        return np.array(out)/np.max(out)
+
+    def get_steps(self):
+        return len(self.history_step)
+
+    def move(self,id):
+        if id==0:
+            return self.move_up()
+        if id==1:
+            return self.move_down()
+        if id==2:
+            return self.move_left()
+        if id==3:
+            return self.move_right()
+
+    def get_history(self):
+        return [np.delete(np.array(self.history_vector),0,0),np.delete(np.array(self.history_step),0,0)]
+
+    def get_max(self):
+        return np.max(self.Matrix)
---
+++ trainer.py
@@ -0,0 +1,121 @@
+import tensorflow as tf
+import numpy as np
+import random as rd
+import Map
+import math
+
+def generate_data(y,times):
+    size=10000
+    maps=[]
+    for i in range(size):
+        maps.append(Map.map())
+
+    for i in range(int(math.pow(2,times))):
+        batch_x=[]
+        for i in maps:
+            batch_x.append(i.get_vector())
+        if times<=3:
+            y__=[rd.randint(0,3) for i in range(len(batch_x))]
+
+        else:
+            y__=[i.tolist().index(np.max(i)) for i in sess.run(y, feed_dict={x:np.array(batch_x)})]
+        # print(y__)
+        # print(sess.run(w1))
+        idx=0
+        while idx <len(y__):
+            move=maps[idx].move(y__[idx])
+            if move:
+                maps.pop(idx)
+                y__.pop(idx)
+            else:
+                idx+=1
+    out=[]
+    r=0
+    for map in maps:
+        if map.get_max() >= object_[times]:
+            r+=1
+            out.append(map.get_history())
+
+    return out,r/len(maps)
+
+def dropout(batchs):
+    for idx in range(len(batchs)):
+        v,l=batchs[idx]
+        tmp=np.zeros(4)
+        for kk in l:
+            tmp+=kk
+        for k,i in enumerate(tmp):
+            if i==0:
+                tmp[k]=999999
+        id=tmp.tolist().index(np.min(tmp))
+        idx_=0
+        while idx_<len(v):
+            if l[idx_].tolist().index(max(l[idx_].tolist()))!=id:
+                # print(l[idx_],id)
+                v=np.delete(v,idx_,0)
+                l = np.delete(l, idx_, 0)
+            else:
+                idx_+=1
+        batchs[idx]=[v,l]
+    return batchs
+
+
+w1=tf.get_variable('w1',[16,16],initializer=tf.random_normal_initializer())
+w2=tf.get_variable('w2',[16,32],initializer=tf.random_normal_initializer())
+w3=tf.get_variable('w3',[32,4],initializer=tf.random_normal_initializer())
+b1=tf.get_variable('b1',[16],initializer=tf.random_normal_initializer())
+b2=tf.get_variable('b2',[32],initializer=tf.random_normal_initializer())
+b3=tf.get_variable('b3',[4],initializer=tf.random_normal_initializer())
+
+x=tf.placeholder(tf.float32,[None,16],name='x-input')
+y_=tf.placeholder(tf.float32,[None,4],name='y-input')
+
+a=tf.nn.relu(tf.matmul(x,w1)+b1)
+b = tf.nn.relu(tf.matmul(a, w2) + b2)
+y=tf.matmul(b, w3) + b3
+
+loss=tf.nn.softmax_cross_entropy_with_logits_v2(logits=y,labels=y_)
+train_step=tf.train.AdadeltaOptimizer(1).minimize(loss)
+saver=tf.train.Saver()
+
+object_={}
+for i in range(1,12):
+    object_[i]=math.pow(2,i+1)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    ob=1
+    r = 0
+    a = 0
+    while True:
+        batchs,ss=generate_data(y,ob)
+        # batchs=dropout(batchs)
+        for xs,ys in batchs:
+            sess.run(train_step,feed_dict={x:xs,y_:ys})
+            if ob<=3:
+                y__=sess.run(y,feed_dict={x:xs})
+                for idx,i in enumerate(y__):
+                    if i.tolist().index(np.max(i))==ys[idx].tolist().index(np.max(ys[idx])):
+                        r+=1
+                    a+=1
+        if ss >0.8 or r/a>0.9:
+            saver.save(sess, './model/model.ckpt')
+            ob+=1
+        try:
+            print(batchs[0], ss,r/a)
+        except:
+            print(batchs,ss,r/a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
